5fd9a07c3f2cde4ecae8556ccaee76df1b6c2b58
Michael Liendo <model3volution@gmail.com>
2016-09-12 16:21:33 -0500

    added access-control headers

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ var jsonParser = require('body-parser').json();
 var router = require('./routes');
 
 app.use('/',express.static(__dirname + '/public'));
+app.use(function(req,res,next) {
+  //it's ok to make requests to this API from any domain
+  res.header("Access-Control-Allow-Origin","*");
+  //tells the client which headers are permitted in the request
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept");
+
+  //grant preflight requests
+  if(req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', "PUT, POST,DELETE");
+    return res.staus(200).json({});
+  }
+  next();
+});
 app.use('/people',jsonParser);
 
 app.use('/people',router);
