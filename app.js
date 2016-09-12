319a4f585082e3181fb0c69dfb1e64ca8dc7615e
Michael Liendo <model3volution@gmail.com>
2016-09-12 13:46:44 -0500

    add bodyparser and post route

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var express = require('express');
 var app = express();
+var jsonParser = require('body-parser').json();
+
+var people = [];
+var bodyID = 1;
+app.use('/people',jsonParser);
 app.get('/',function(req,res) {
-  res.send('All good over here!');
+  res.send('This is going to be my main page');
+});
+
+app.get('/people',function(req,res) {
+  res.send(people);
+});
+
+app.post('/people',function(req,res) {
+  var body = req.body;
+  body.id = bodyID;
+  bodyID++;
+  
+  people.push(body);
+  res.send(people);
 });
 
 app.listen(3000,function() {
