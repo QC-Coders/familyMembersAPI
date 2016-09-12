11d73863d3d301a213ede08291ee7884a45eb2f7
Michael Liendo <model3volution@gmail.com>
2016-09-12 14:35:26 -0500

    added public folder with express.static

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ var app = express();
 var jsonParser = require('body-parser').json();
 var router = require('./routes');
 
+app.use('/',express.static(__dirname + '/public'));
 app.use('/people',jsonParser);
-app.get('/',function(req,res) {
-  res.send('This is going to be my main page');
-});
 
 app.use('/people',router);
 app.listen(3000,function() {
